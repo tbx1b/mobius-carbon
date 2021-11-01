004f0384e810d29ed3fd2b6fa8df541fd2fcbbfc
MOBIUSLOOPFOUR <scratch.joint-0i@icloud.com>
2021-11-01 21:51:05 +0100

    Documentation

---
+++ man/template/__name__.c
@@ -0,0 +1,33 @@
+/*
+ * MODULE:          __MODULE__
+ * LICENSE:         __LICENSE__
+ * PURPOSE:         __PURPOSE__
+ * COPYRIGHT:       __AUTHOR__
+ */
+
+#ifndef name_h_
+#define name_h_
+
+/* INCLUDES *******************************************************************/
+
+#include <__name__.h>
+#warning Template in source files.
+
+/* GLOBAL FUNCTIONS ***********************************************************/
+
+type_t 
+module_subm_ident(int x)
+{
+    /* code */
+}
+
+/* PRIVATE FUNCTIONS **********************************************************/
+
+static type_t 
+ident_helper(int x) /* doesn't need a namespace */
+{
+    /* code */
+}
+
+#endif /* name_h_ */
+/*! EOF */
