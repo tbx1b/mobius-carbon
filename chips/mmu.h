d12ca9e779af5cf9108d54414f114d8e2b178e7a
MOBIUSLOOPFOUR <scratch.joint-0i@icloud.com>
2021-11-15 01:34:52 +0100

    License and file headers

--- chips/mmu.h
+++ chips/mmu.h
@@ -1,5 +1,21 @@
-#ifndef mmtab_h_
-#define mmtab_h_
+// =========-------------------------- mmu.h  ------------------------=========//
+//                       Copyright 2021 MobiusLoopFour
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// =========----------------------------------------------------------=========// 
+
+#ifndef mmu_h_
+#define mmu_h_
 
 #define FL_IF           0x00000200
 #define DPL_USER        0x3
@@ -141,4 +157,4 @@ struct gatedesc {
 }
 #endif /* __ASSEMBLER__ */
 
-#endif /* mmtab_h_ */
+#endif /* mmu_h_ */
